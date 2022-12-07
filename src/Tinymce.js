e07705f20581f4a4a799e77da0bb8c9e0df56df1
JaeGeunBang <jgb710@naver.com>
2022-12-07 15:30:12 +0900

    메일 추가

--- src/Tinymce.js
+++ src/Tinymce.js
@@ -18,18 +18,19 @@ function Tinymce() {
                     height: 500,
                     menubar: false,
                     plugins: [
-                        'advlist autolink lists link image charmap print preview anchor',
-                        'searchreplace visualblocks code fullscreen',
-                        'insertdatetime media table paste code help wordcount'
+                        'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+                        'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+                        'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
                     ],
-                    toolbar: 'undo redo | formatselect | ' +
-                        'bold italic backcolor | alignleft aligncenter ' +
-                        'alignright alignjustify | bullist numlist outdent indent | ' +
-                        'removeformat | help',
+                    toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
+                        'alignleft aligncenter alignright alignjustify | ' +
+                        'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
                     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                 }}
             />
             <button onClick={log}>Log editor content</button>
+            <br/>
+            {editorRef.current.getContent()}
         </>
     );
 }
--- src/pages/MailBoxPage.js
+++ src/pages/MailBoxPage.js
@@ -1,15 +1,34 @@
 import { Helmet } from 'react-helmet-async';
-import { faker } from '@faker-js/faker';
-// @mui
+
 import { useTheme } from '@mui/material/styles';
 import {Grid, Container, Typography, Button} from '@mui/material';
 
-import {useState} from "react";
+import {useRef, useState} from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import Iconify from "../components/iconify";
 
-// ----------------------------------------------------------------------
+import {EditorState, convertToRaw} from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import styled from 'styled-components';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import draftToHtml from 'draftjs-to-html';
+import Tinymce from "../Tinymce";
+
+const MyBlock = styled.div`
+    .wrapper-class{
+        width: 100%;
+        margin: 0 auto;
+        margin-bottom: 4rem;
+    }
+  .editor {
+    height: 500px !important;
+    width: 100% !important;
+    border: 1px solid #f1f1f1 !important;
+    padding: 5px !important;
+    border-radius: 2px !important;
+  }
+`;
 
 export default function MailBoxPage() {
   const theme = useTheme();
@@ -28,6 +47,17 @@ export default function MailBoxPage() {
     const [value, setValue] = useState('');
 
 
+    const [editorState, setEditorState] = useState(() =>
+        EditorState.createEmpty()
+    );
+
+    const onEditorStateChange = (editorState) => {
+        setEditorState(editorState);
+    };
+
+
+    const editorRef = useRef(null);
+
     return (
     <>
       <Helmet>
@@ -36,7 +66,7 @@ export default function MailBoxPage() {
 
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome MailBox
+          1. Quill
         </Typography>
 
         <Grid container spacing={10}>
@@ -57,6 +87,39 @@ export default function MailBoxPage() {
             <Grid item xs={12}>
                 {value}
             </Grid>
+        </Grid>
+
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          2. DraftJs
+        </Typography>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+              <MyBlock>
+                  <Editor
+                      wrapperClassName="wrapper-class"
+                      editorClassName="editor"
+                      toolbarClassName="toolbar-class"
+                      toolbar={{
+                          list: { inDropdown: true },
+                          textAlign: { inDropdown: true },
+                          link: { inDropdown: true },
+                          history: { inDropdown: false },
+                      }}
+                      placeholder=""
+                      editorState={editorState}
+                      onEditorStateChange={onEditorStateChange}
+                  />
+                  {draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+              </MyBlock>
+          </Grid>
+        </Grid>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          3. Tinymce
+        </Typography>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+              <Tinymce />
+          </Grid>
         </Grid>
       </Container>
     </>
